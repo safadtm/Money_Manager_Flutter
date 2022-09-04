76d80bc21e1c70c2bb49cc8f558e4ce1f10d1eae
safadtm <safadtm2004@gmail.com>
2022-09-04 12:51:19 +0530

    tab bar ui ready

--- lib/screens/category/screen_categories.dart
+++ lib/screens/category/screen_categories.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ScreenCategories extends StatelessWidget {
+class ScreenCategories extends StatefulWidget {
   const ScreenCategories({super.key});
 
   @override
+  State<ScreenCategories> createState() => _ScreenCategoriesState();
+}
+
+class _ScreenCategoriesState extends State<ScreenCategories>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('screen Categories'),
+    return Column(
+      children: [
+        TabBar(
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey,
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: 'INCOME',
+            ),
+            Tab(
+              text: 'EXPENSE',
+            ),
+          ],
+        )
+      ],
     );
   }
 }
