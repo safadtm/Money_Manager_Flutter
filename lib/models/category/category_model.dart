afc14117f974669ba1c4d868df6d70c40f2b57f8
safadtm <safadtm2004@gmail.com>
2022-09-05 07:33:17 +0530

    hive type adapter added

--- lib/models/category/category_model.dart
+++ lib/models/category/category_model.dart
@@ -1,12 +1,23 @@
+import 'package:hive_flutter/hive_flutter.dart';
+part 'category_model.g.dart';
+
 enum CategoryType {
   income,
   expense,
 }
 
+@HiveType(typeId: 1)
 class CategoryModel {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final CategoryType;
+
+  @HiveField(3)
   final bool isDeleted;
 
   CategoryModel({
