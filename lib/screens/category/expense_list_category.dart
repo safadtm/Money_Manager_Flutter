6405b84c3763b4195181995dcc597b6eb5cf2369
safadtm <safadtm2004@gmail.com>
2022-09-05 15:39:50 +0530

    income &expense DB connection set

--- lib/screens/category/expense_list_category.dart
+++ lib/screens/category/expense_list_category.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:money_manager_flutter/db/category/category_db.dart';
+
+import '../../models/category/category_model.dart';
 
 class ExpenseListCategory extends StatelessWidget {
   const ExpenseListCategory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: ListTile(
-              title: Text('Expense $index'),
-              trailing: IconButton(
-                color: Colors.red,
-                hoverColor: Colors.red,
-                onPressed: () {},
-                icon: Icon(
-                  Icons.delete,
+    return ValueListenableBuilder(
+        valueListenable: CategoryDB().expenseListListener,
+        builder: (BuildContext ctx, List<CategoryModel> newList, Widget? _) {
+          return ListView.separated(
+            itemBuilder: (ctx, index) {
+              final category = newList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(category.name),
+                  trailing: IconButton(
+                    color: Colors.red,
+                    hoverColor: Colors.red,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete,
+                    ),
+                  ),
                 ),
-              ),
-            ),
+              );
+            },
+            separatorBuilder: (ctx, index) {
+              return SizedBox(
+                height: 10,
+              );
+            },
+            itemCount: newList.length,
           );
-        },
-        separatorBuilder: (ctx, index) {
-          return SizedBox(
-            height: 10,
-          );
-        },
-        itemCount: 10);
+        });
   }
 }
--- lib/screens/category/income_list_category.dart
+++ lib/screens/category/income_list_category.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../../db/category/category_db.dart';
+import '../../models/category/category_model.dart';
+
 class IncomeListCategory extends StatelessWidget {
   const IncomeListCategory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: ListTile(
-              title: Text('Income $index'),
-              trailing: IconButton(
-                color: Colors.red,
-                hoverColor: Colors.red,
-                onPressed: () {},
-                icon: Icon(Icons.delete),
-              ),
-            ),
+    return ValueListenableBuilder(
+        valueListenable: CategoryDB().incomeListListener,
+        builder: (BuildContext ctx, List<CategoryModel> newList, Widget? _) {
+          return ListView.separated(
+            itemBuilder: (ctx, index) {
+              final category = newList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(category.name),
+                  trailing: IconButton(
+                    color: Colors.red,
+                    hoverColor: Colors.red,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete,
+                    ),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (ctx, index) {
+              return SizedBox(
+                height: 10,
+              );
+            },
+            itemCount: newList.length,
           );
-        },
-        separatorBuilder: (ctx, index) {
-          return SizedBox(
-            height: 10,
-          );
-        },
-        itemCount: 10);
+        });
   }
 }
